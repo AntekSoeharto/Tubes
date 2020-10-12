033c631ef49224a92650e8ce2a4b5b840484c209
hansen <hansen @hansen>
2020-10-12 13:43:47 +0700

    bwt enum StatusAbsensi dan update AbsensiDokter

--- src/Model/AbsensiDokter.java
+++ src/Model/AbsensiDokter.java
@@ -13,13 +13,13 @@ import java.util.Date;
  */
 public class AbsensiDokter implements InterfaceAbsensiDokter{
     private Date tanggal;
-    private int Status;
+    private StatusAbsensi Status;
     
     public AbsensiDokter(){
         
     }
 
-    public AbsensiDokter(Date tanggal, int Status) {
+    public AbsensiDokter(Date tanggal, StatusAbsensi Status) {
         this.tanggal = tanggal;
         this.Status = Status;
     }
@@ -32,11 +32,11 @@ public class AbsensiDokter implements InterfaceAbsensiDokter{
         this.tanggal = tanggal;
     }
 
-    public int getStatus() {
+    public StatusAbsensi getStatus() {
         return Status;
     }
 
-    public void setStatus(int Status) {
+    public void setStatus(StatusAbsensi Status) {
         this.Status = Status;
     }
 
