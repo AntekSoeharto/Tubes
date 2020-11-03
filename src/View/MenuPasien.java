e622e7050dce6a5330afce3a793d423722b8c209
hp <hp@AndreasJumaga>
2020-11-03 20:03:14 +0700

    Menambahkan Class Menu Dokter, pasien dan admin

---
+++ src/View/MenuPasien.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author hp
+ */
+public class MenuPasien implements ActionListener{
+    
+    JFrame frame = new JFrame("MainMenu");
+    JPanel menu = new JPanel();
+    JPanel isi = new JPanel();
+    
+    JButton menu_pasien = new JButton("PASIEN");
+    JButton menu_dokter = new JButton("DOKTER");
+    JButton menu_admin = new JButton("ADMINISTRASI");
+    
+    public MenuPasien(){
+        
+        frame.setSize(1200, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        menu.setLayout(null);
+        isi.setLayout(null);
+        menu.setBounds(10,10,200,640);
+        isi.setBounds(230,10,930,640);
+        isi.setBackground(Color.cyan);
+        menu.setBackground(Color.cyan);
+        menu_pasien.setBounds(35,200,120,50);
+        menu_dokter.setBounds(35,260,120,50);
+        menu_admin.setBounds(35,320,120,50);
+        menu.add(menu_dokter);
+        menu.add(menu_pasien);
+        menu.add(menu_admin);
+        menu_dokter.addActionListener(this);
+        menu_pasien.addActionListener(this);
+        menu_admin.addActionListener(this);
+        
+        
+        frame.add(isi);
+        frame.add(menu);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        String command = ae.getActionCommand();
+        switch(command) {
+            case "PASIEN": 
+                new MenuPasien();
+                frame.setVisible(false);
+                break;
+            case "UPDATE":
+                new MenuDokter();
+                frame.setVisible(false);
+                break;
+            case "DELETE":
+                new MenuAdmin();
+                frame.setVisible(false);
+                break;
+            default: 
+                break;
+        }
+    }
+}
