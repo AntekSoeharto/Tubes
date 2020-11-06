857cbc6d2a7fa45e8018b662576ff2ec90961967
alexander-hansen <71882433+alexander-hansen@users.noreply.github.com>
2020-11-06 23:41:18 +0700

    Revert "Hansen"

---
+++ src/Main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+import Model.*;
+import java.util.ArrayList;
+import java.util.Date;
+import Controller.Controller;
+/**
+ *
+ * @author hp
+ */
+import View.LoginScreen;
+public class Main {
+    public static void main(String[] args){
+        new LoginScreen();
+    }
+}
