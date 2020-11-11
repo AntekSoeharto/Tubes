be51688a2cbbcf6d9e966ee75fe41e86ab5cbf1e
hp <hp@AndreasJumaga>
2020-11-11 11:08:04 +0700

    Memperbaiki Class ControllerStaff

--- src/Controller/ControllerStaff.java
+++ src/Controller/ControllerStaff.java
@@ -5,10 +5,34 @@
  */
 package Controller;
 
+import static Controller.ControllerDokter.conn;
+import Model.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
 /**
  *
  * @author V for Vladimir
  */
 public class ControllerStaff {
     
+    public static Staff getStaff(String username){
+        conn.connect();
+        Staff staff = new Staff();
+        String query = "SELECT * FROM staff WHERE username='" + username + "'";
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                staff.setUsername(rs.getString("username"));
+                staff.setPassword(rs.getString("password"));
+                staff.setIdCabang(rs.getString("ID_cabang"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (staff);
+    }
 }
