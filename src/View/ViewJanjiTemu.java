aed26b9236f55e975f4c470e974ca4a1a70f5317
hansen <hansen @hansen>
2020-11-17 20:14:57 +0700

    create ViewJanjiTemu

---
+++ src/View/ViewJanjiTemu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author V for Vladimir
+ */
+public class ViewJanjiTemu {
+    JFrame janjiTemu = new JFrame();
+    JLabel NIKLabel,poliLabel,dokterLabel,hargaLabel;
+    JTextField NIKField,poliField,dokterField;
+    JTable daftarDokter;
+    JButton buttonNext,buttonBuatJanji;
+    public ViewJanjiTemu(){
+        
+    }
+}
