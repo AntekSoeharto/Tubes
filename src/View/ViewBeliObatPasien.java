b1fc01aa336375ca25d71b6851de5b3721c1849c
hansen <hansen @hansen>
2020-11-21 17:54:36 +0700

    update ViewBeliObatPasien

--- src/View/ViewBeliObatPasien.java
+++ src/View/ViewBeliObatPasien.java
@@ -30,26 +30,34 @@ import javax.swing.WindowConstants;
  *
  * @author V for Vladimir
  */
-public class ViewBeliObatPasien {
+public class ViewBeliObatPasien implements ActionListener{
     Transaksi t = new Transaksi();
     JFrame formBeliObat = new JFrame();
-    JLabel jumlahJenisObatLabel,namaObatLabel,totalHarga,jumlahObatLabel;
+    JLabel jumlahJenisObatLabel,totalHarga;
+    JLabel[] namaObatLabel,jumlahObatLabel;
     JTextField jumlahJenisObatField;
     JTextField[] namaObatFields,jumlahObatFields;
-    JButton buttonHitungTotalHarga,buttonLanjut;
+    JButton buttonHitungTotalHarga,buttonLanjut,buttonPrev;
     JPanel panelContent,panelMenu;
     JButton menu_pasien = new JButton("PASIEN");
     JButton menu_dokter = new JButton("DOKTER");
     JButton menu_admin = new JButton("ADMINISTRASI");
-    public ViewBeliObatPasien(String NIK,double hargaKonsul){
+    String NIKInput;
+    double hargaKonsul;
+    public ViewBeliObatPasien(String NIK,double hargaKonsultasi){
+        NIKInput = NIK;
+        hargaKonsul = hargaKonsultasi;
         formBeliObat.setSize(1200, 700);
         jumlahJenisObatLabel = new JLabel("jumlah Jenis obat");
         jumlahJenisObatField = new JTextField();
         buttonLanjut = new JButton("Lanjut");
         buttonHitungTotalHarga = new JButton("hitung total harga");
+        buttonPrev = new JButton("Kembali");
         panelContent = new JPanel();
         panelMenu = new JPanel();
         
+        buttonPrev.setVisible(false);
+        buttonHitungTotalHarga.setVisible(false);
         panelContent.setLayout(null);
         panelMenu.setLayout(null);
         
@@ -61,6 +69,7 @@ public class ViewBeliObatPasien {
         jumlahJenisObatLabel.setBounds(10, 40, 140, 25);
         jumlahJenisObatField.setBounds(160, 40, 140, 25);
         buttonLanjut.setBounds(265, 400, 150, 25);
+        buttonPrev.setBounds(600, 500, 100, 25);
         buttonHitungTotalHarga.setBounds(740, 500, 150, 25);
         
         menu_pasien.setBounds(35,200,120,50);
@@ -71,8 +80,7 @@ public class ViewBeliObatPasien {
         panelContent.add(jumlahJenisObatField);
         panelContent.add(buttonLanjut);
         panelContent.add(buttonHitungTotalHarga);
-        panelContent.add(buttonLanjut);
-        panelContent.add(buttonLanjut);
+        panelContent.add(buttonPrev);
         panelMenu.add(menu_pasien);
         panelMenu.add(menu_dokter);
         panelMenu.add(menu_admin);
@@ -81,32 +89,60 @@ public class ViewBeliObatPasien {
         formBeliObat.add(panelContent);
         formBeliObat.setLayout(null);
         formBeliObat.setVisible(true);
-        formBeliObat.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        buttonLanjut.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        formBeliObat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        buttonLanjut.addActionListener(this);
+        buttonHitungTotalHarga.addActionListener(this);
+        buttonPrev.addActionListener(this);
+        
+        menu_dokter.addActionListener(this);
+        menu_pasien.addActionListener(this);
+        menu_admin.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch(command) {
+            case "PASIEN": 
+                new MenuPasien();
+                formBeliObat.setVisible(false);
+                break;
+            case "DOKTER":
+                new MenuDokter();
+                formBeliObat.setVisible(false);
+                break;
+            case "ADMINISTRASI":
+                new MenuAdmin();
+                formBeliObat.setVisible(false);
+                break;
+            case "Lanjut":
                 jumlahJenisObatLabel.setVisible(false);
                 jumlahJenisObatField.setVisible(false);
+                buttonHitungTotalHarga.setVisible(true);
+                buttonPrev.setVisible(true);
                 buttonLanjut.setVisible(false);
                 try {
                     String jmlStr = jumlahJenisObatField.getText();
                     int jml = Integer.parseInt(jmlStr);
                     namaObatFields = new JTextField[jml];
+                    namaObatLabel = new JLabel[jml];
+                    jumlahObatLabel = new JLabel[jml];
                     jumlahObatFields = new JTextField[jml];
                     for(int i = 0; i < jml; i++){
-                        namaObatLabel = new JLabel("nama obat " + (i+1));
-                        jumlahObatLabel = new JLabel("jumlah obat " + (i+1) + "(dalam mg)");
+                        namaObatLabel[i] = new JLabel("nama obat " + (i+1));
+                        jumlahObatLabel[i] = new JLabel("jumlah obat " + (i+1) + "(dalam mg)");
                         namaObatFields[i] = new JTextField();
                         jumlahObatFields[i] = new JTextField();
                         
-                        namaObatLabel.setBounds(10, (i*3+1)*10, 100, 25);
+                        namaObatLabel[i].setBounds(10, (i*3+1)*10, 100, 25);
                         namaObatFields[i].setBounds(130, (i*3+1)*10, 120, 25);
-                        jumlahObatLabel.setBounds(10, (i*3+1)*10, 180, 25);
-                        jumlahObatFields[i].setBounds(210, (i*3+1)*10, 120, 25);
+                        jumlahObatLabel[i].setBounds(270, (i*3+1)*10, 180, 25);
+                        jumlahObatFields[i].setBounds(470, (i*3+1)*10, 120, 25);
                         
-                        panelContent.add(namaObatLabel);
+                        panelContent.add(namaObatLabel[i]);
                         panelContent.add(namaObatFields[i]);
-                        panelContent.add(jumlahObatLabel);
+                        panelContent.add(jumlahObatLabel[i]);
                         panelContent.add(jumlahObatFields[i]);
                     }
                 } catch (Exception exception) {
@@ -115,14 +151,14 @@ public class ViewBeliObatPasien {
                         jumlahJenisObatField.setText("");
                     }
                 }
-            }
-        });
-        buttonHitungTotalHarga.addActionListener(new ActionListener() {
-             @Override
-             public void actionPerformed(ActionEvent e) {
-                 int hargaKonsul = 15000;
+                panelContent.validate();
+                panelContent.repaint();
+                break;
+            case "hitung total harga":
+                new ViewSearchPasien();
+                int hargaKonsul = 15000;
                 int golonganPasien = 0;
-                if(ControllerPasien.getPasien(NIK).getBPJS() == GolonganPasien.BPJS){
+                if(ControllerPasien.getPasien(NIKInput).getBPJS() == GolonganPasien.BPJS){
                     golonganPasien = 1;
                 }else{
                     golonganPasien = 2;
@@ -146,7 +182,27 @@ public class ViewBeliObatPasien {
                 t.setTanggakMasuk(hariIni);
                 t.setTotal(totalHarga + hargaKonsul);
                  ControllerTransaksi.insertNewTransaksi(t);
-            }
-        });
+                break;
+            case "Kembali" :
+                jumlahJenisObatLabel.setVisible(true);
+                jumlahJenisObatField.setVisible(true);
+                buttonLanjut.setVisible(true);
+                buttonPrev.setVisible(false);
+                buttonHitungTotalHarga.setVisible(false);
+                if(namaObatFields.length > 0){
+                    for(int i = 0 ;i < namaObatFields.length ;i++){
+                        namaObatFields[i].setVisible(false);
+                        namaObatLabel[i].setVisible(false);
+                        jumlahObatFields[i].setVisible(false);
+                        jumlahObatLabel[i].setVisible(false);
+                        panelContent.remove(namaObatFields[i]);
+                        panelContent.remove(namaObatLabel[i]);
+                        panelContent.remove(jumlahObatLabel[i]);
+                        panelContent.remove(jumlahObatFields[i]);
+                    }
+                }
+            default: 
+                break;
+        }
     }
 }
