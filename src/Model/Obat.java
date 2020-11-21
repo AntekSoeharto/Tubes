d62edb90d202156f6bfcfb8f15dd424732198aec
hansen <hansen @hansen>
2020-11-21 16:45:39 +0700

    update Obat

--- src/Model/Obat.java
+++ src/Model/Obat.java
@@ -10,6 +10,8 @@ package Model;
  * @author hp
  */
 public class Obat {
+    private String IDObat;
+    private String IDMasaLakuObat;
     private String Nama;
     private int hargaBeli;
     private int Stok;
@@ -31,6 +33,33 @@ public class Obat {
         this.tgl_kadaluarsa = tgl_kadaluarsa;
     }
 
+    public Obat(String IDObat, String IDMasaLakuObat, String Nama, int hargaBeli, int Stok, int hargaJual, String tgl_beli, String tgl_kadaluarsa) {
+        this.IDObat = IDObat;
+        this.IDMasaLakuObat = IDMasaLakuObat;
+        this.Nama = Nama;
+        this.hargaBeli = hargaBeli;
+        this.Stok = Stok;
+        this.hargaJual = hargaJual;
+        this.tgl_beli = tgl_beli;
+        this.tgl_kadaluarsa = tgl_kadaluarsa;
+    }
+    
+    public String getIDObat() {
+        return IDObat;
+    }
+
+    public void setIDObat(String IDObat) {
+        this.IDObat = IDObat;
+    }
+
+    public String getIDMasaLakuObat() {
+        return IDMasaLakuObat;
+    }
+
+    public void setIDMasaLakuObat(String IDMasaLakuObat) {
+        this.IDMasaLakuObat = IDMasaLakuObat;
+    }
+    
     public String getTgl_beli() {
         return tgl_beli;
     }
